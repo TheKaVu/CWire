a940d435ada0a10e4e96989956c246b564f31a7b
Kamil Więczaszek <k.wieczaszek@gmail.com>
2026-06-13 00:00:14 +0200

    Fixed copying, moving and equality operator in delegate.hpp

--- src/delegate.hpp
+++ src/delegate.hpp
@@ -11,44 +11,54 @@ namespace cwr {
 
         ContextFunc_t m_Function = nullptr;
         void* m_Context = nullptr;
+        uintptr_t m_MemRef = 0;
 
-        delegate(const ContextFunc_t function, void* context) : m_Function(function), m_Context(context) {}
+        delegate(const ContextFunc_t function, void* context, const uintptr_t memRef) : m_Function(function), m_Context(context), m_MemRef(memRef) {}
 
     public:
         delegate() = default;
 
         delegate(const delegate& other) {
             m_Function = other.m_Function;
+            m_Context = other.m_Context;
+            m_MemRef = other.m_MemRef;
         }
 
-        delegate(const delegate&& other) noexcept {
+        delegate(delegate&& other) noexcept {
             m_Function = std::move(other.m_Function);
+            m_Context = other.m_Context;
+            other.m_Context = nullptr;
+            m_MemRef = other.m_MemRef;
+            other.m_MemRef = 0;
         }
 
         virtual ~delegate(){
             m_Function = nullptr;
             m_Context = nullptr;
+            m_MemRef = 0;
         }
 
         template<R(*Func)(Args...)>
         void set() {
             m_Context = nullptr;
             m_Function = [](void*, Args... args) {return Func(std::forward<Args>(args)...); };
+            m_MemRef = reinterpret_cast<uintptr_t>(Func);
         }
 
         template<typename T, R(T::*Func)(Args...)>
-        void set(T& context) {
+        void set(const T& context) {
             m_Context = context;
             m_Function = [](void* ctx, Args... args) {
                 return (static_cast<T*>(ctx)->*Func)(std::forward<Args>(args)...);
             };
+            m_MemRef = reinterpret_cast<uintptr_t>(Func);
         }
 
-        R invoke(Args... args) {
+        R invoke(Args... args) const {
             return (*m_Function)(m_Context, args...);
         }
 
-        R operator ()(Args... args) {
+        R operator ()(Args... args) const {
             return invoke(std::forward<Args>(args)...);
         }
 
@@ -56,28 +66,32 @@ namespace cwr {
 
         constexpr delegate& operator =(delegate&& other) noexcept {
             m_Function = std::move(other.m_Function);
+            m_Context = other.m_Context;
+            other.m_Context = nullptr;
+            m_MemRef = other.m_MemRef;
+            other.m_MemRef = 0;
             return *this;
         }
 
-        bool operator ==(const delegate& handler) const {
-            return m_Function == handler.m_Function;
+        bool operator ==(const delegate& other) const {
+            return m_Context == other.m_Context && m_MemRef == other.m_MemRef;
         }
 
         template<typename OtherR, typename ...OtherArgs, typename Other = delegate<OtherR, OtherArgs...>>
         operator delegate<OtherR, OtherArgs...>() const {
-            return Other(reinterpret_cast<typename Other::ContextFunc_t>(m_Function));
+            return Other(reinterpret_cast<Other::ContextFunc_t>(m_Function), m_Context, m_MemRef);
         }
 
         template<R(*Func)(Args...)>
         static delegate of() {
-            return delegate([](void*, Args... args) {return Func(std::forward<Args>(args)...); }, nullptr);
+            return delegate([](void*, Args... args) {return Func(std::forward<Args>(args)...); }, nullptr, reinterpret_cast<uintptr_t>(Func));
         }
 
         template<typename T, R(T::*Func)(Args...)>
-        static delegate of(T& context) {
+        static delegate of(const T& context) {
             return delegate([](void* ctx, Args... args) {
                 return (static_cast<T*>(ctx)->*Func)(std::forward<Args>(args)...);
-            }, &context);
+            }, &context, reinterpret_cast<uintptr_t>(Func));
         }
     };
 
