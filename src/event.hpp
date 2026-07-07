0b5b05ff5144ca94b5c12f3139a6f01b79d45517
Kamil Więczaszek <k.wieczaszek@gmail.com>
2026-07-08 00:52:22 +0200

    Added Doxygen for event class.

--- src/event.hpp
+++ src/event.hpp
@@ -4,6 +4,9 @@
 #include "multidelegate.hpp"
 
 namespace cwr {
+
+    /// Base class for an event.
+    /// @tparam Args Argument types.
     template <typename ...Args>
     class event : public multidelegate<void, Args...>{
 
@@ -20,6 +23,10 @@ namespace cwr {
     };
 }
 
+/// Creates an event, special void-returning multidelegate callable only inside the class it's declared in.
+/// @param name Name of the event.
+/// @param parent Parent class.
+/// @param args_t Argument types.
 #define EVENT(name, parent, args_t...) protected:\
 class event_##name : public event<args_t> {\
 friend parent;\
